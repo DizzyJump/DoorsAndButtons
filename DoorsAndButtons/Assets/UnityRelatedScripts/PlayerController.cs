1506baa7f7dbc1d8314a1f5e9b8147173eba39bc
Sergey Stolnikov <mojpovelitel@gmail.com>
2022-08-27 21:52:38 +0300

    Add rotation and animations to player model

---
+++ DoorsAndButtons/Assets/UnityRelatedScripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] AnimatorController idleAnimation;
+    [SerializeField] AnimatorController walkAnimation;
+
+    Animator animator;
+    Vector3 prevPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPosition = transform.position;
+        var direction = currentPosition - prevPosition;
+        transform.LookAt(transform.position + direction, Vector3.up);
+        if (direction.sqrMagnitude > float.Epsilon)
+        {
+            if (animator.runtimeAnimatorController != walkAnimation)
+                animator.runtimeAnimatorController = walkAnimation;
+        }
+        else
+        {
+            if (animator.runtimeAnimatorController != idleAnimation)
+                animator.runtimeAnimatorController = idleAnimation;
+        }
+        prevPosition = currentPosition;
+    }
+}
